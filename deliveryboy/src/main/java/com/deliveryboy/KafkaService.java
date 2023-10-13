aafee41c3480c1edf121178d41ad698a2165cf7b
Zaid Ansari <mohdzaidansari010@mail.com>
2023-10-13 13:27:38 +0530

    Apache Kafka Implementation

---
+++ deliveryboy/src/main/java/com/deliveryboy/KafkaService.java
@@ -0,0 +1,22 @@
+package com.deliveryboy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaService {
+	
+	@Autowired
+	KafkaTemplate<String, String> kafkaTemplate;
+
+	private Logger logger = LoggerFactory.getLogger(KafkaService.class);
+	
+	public boolean updateLocation(String location) {
+		kafkaTemplate.send("location-update-topic", location);
+		logger.info("Message Produced");
+		return true;
+	}
+}
